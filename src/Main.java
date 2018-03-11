c7d5cac377dea3a3dff464f787f9778b3631bf8a
hanielhgc <haniel_sasuke@hotmail.com>
2018-03-11 18:00:54 -0300

    Ideia do Buffer

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,14 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-
-
 		// eliude soares teixeira de souza
 		int n = 200;
 
-		byte tipoBloco=0;
-		byte contContainer0=0;
-		
-		int contContainer=0;
-		int contBlocos=0;
+		byte tipoBloco = 0;
+		byte contContainer0 = 0;
+
+		int contContainer = 0;
+		int contBlocos = 0;
 
 		int contByte;
 
@@ -27,18 +25,13 @@ public class Main {
 
 		byte conteudo2[];
 
-
-		//conteudo2 = conteudo.getBytes();
+		// conteudo2 = conteudo.getBytes();
 		int ultimoI = 0;
-		
-		byte[] blocoFinal = new byte[n+9];
 
+		byte[] blocoFinal = new byte[n + 8];
 
 		ArrayList<byte[]> dadosAll = new ArrayList();
 		ArrayList<byte[]> dadosFinaisAll = new ArrayList();
-
-
-
 
 		Scanner scanner = new Scanner(System.in);
 		String conteudo = "";
@@ -56,6 +49,9 @@ public class Main {
 			BufferedReader lerArq = new BufferedReader(arq);
 
 			String linha = lerArq.readLine(); // lê a primeira linha
+
+			// armazenar aqui no bloco de controle
+
 			// a variável "linha" recebe o valor "null" quando o processo
 			// de repetição atingir o final do arquivo texto
 			while (linha != null) {
@@ -63,36 +59,29 @@ public class Main {
 				conteudo = conteudo + "\n" + linha;
 
 				linha = lerArq.readLine(); // lê da segunda até a última linha
-			
-				byte meta[] = new byte[9];
+
+				byte meta[] = new byte[8];
 				byte conteudo3[] = new byte[n];
 				System.out.println(linha);
-				if(linha!= null) {
-				conteudo3=linha.getBytes();
-				meta=metaBlocoDeDados(contContainer0, Integer.toString(contBlocos).getBytes(), tipoBloco, Integer.toString(conteudo3.length).getBytes());
-				blocoFinal = vectorMerge(meta, conteudo3);
-				contBlocos++;
-						
-				}
-				//***adicionar aqui
-				
+				if (linha != null) {
+					conteudo3 = linha.getBytes();
+					meta = metaBlocoDeDados(contContainer0, Integer.toString(contBlocos).getBytes(), tipoBloco,
+							Integer.toString(conteudo3.length).getBytes());
+					blocoFinal = vectorMerge(meta, conteudo3);
+					contBlocos++;
 
-				if(conteudo3!= null) {
-					
-				
-				dadosAll.add(conteudo3);
-				dadosFinaisAll.add(blocoFinal);
-				//testar
-				
-				
 				}
-				
-				
+				// ***adicionar aqui
 
-					
-				
-				
-				
+				if (conteudo3 != null) {
+
+					dadosAll.add(conteudo3);
+					dadosFinaisAll.add(blocoFinal);
+
+					// testar
+
+				}
+
 			}
 
 			arq.close();
@@ -100,21 +89,13 @@ public class Main {
 			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
 		}
 
-		
-		
-
-		
-
 		/*
-		armazenar(ultimoI, conteudo2, n, dadosAll, contBlocos);
-
-		for (int i = 0; i < dadosAll.get(1).length; i++) {
-			System.out.println(dadosAll.get(1)[i]);
-		}
-
-		*/
-
-		
+		 * armazenar(ultimoI, conteudo2, n, dadosAll, contBlocos);
+		 * 
+		 * for (int i = 0; i < dadosAll.get(1).length; i++) {
+		 * System.out.println(dadosAll.get(1)[i]); }
+		 * 
+		 */
 
 		// String msgDecode = new String(dados, "UTF-8");
 
@@ -125,69 +106,77 @@ public class Main {
 		}
 
 		// System.out.println(msgDecode);
-		
-		
-		
-		
-		
+
 		imprimirVetor(blocoFinal);
 
-	}
-	
-	public static void imprimirVetor(byte vetor[]){
+		System.out.println("É AQUI Ó!!!!!!");
+		for (int i = 0; i < dadosFinaisAll.size(); i++) {
+			for (int j = 0; j < dadosFinaisAll.get(i).length; j++) {
+				System.out.println(dadosFinaisAll.get(i)[j]);
+
+			}
+
+		}
 		
-		for (int i = 0; i < vetor.length; i++) {
+		
+		
+		Buffer[] buffer = new Buffer[10];
+		
+
+	}
+
+	public static void imprimirVetor(byte vetor[]) {
+
+		for (int i = 0; i < vetor.length - 1; i++) {
 			System.out.println(vetor[i]);
 		}
-		
+
 	}
-	
-	public static byte[] vectorMerge(byte vetor1[], byte vetor2[]){
-byte vetorFinal[] = new byte[vetor1.length+vetor2.length];
-		
+
+	public static byte[] vectorMerge(byte vetor1[], byte vetor2[]) {
+		byte vetorFinal[] = new byte[vetor1.length + vetor2.length];
+
 		for (int i = 0; i < vetor1.length; i++) {
-			vetorFinal[i]=vetor1[i];
-			
+			vetorFinal[i] = vetor1[i];
+
 		}
-		
+
 		for (int i = 0; i < vetor2.length; i++) {
-			vetorFinal[i+vetor1.length]=vetor2[i];
+			vetorFinal[i + vetor1.length - 1] = vetor2[i];
 		}
-		
-		
+
 		return vetorFinal;
 	}
-	
+
 	public static byte[] metaBlocoDeDados(byte idContainer, byte[] idBloco, byte tipo, byte[] utilizado) {
-		
-		
+
 		byte metadados[] = new byte[9];
-		metadados[0]=idContainer;
-		
+		metadados[0] = idContainer;
+
 		for (int i = 0; i < idBloco.length; i++) {
-			metadados[i+1]=idBloco[i];
+			metadados[i + 1] = idBloco[i];
 		}
-		
-		metadados[4]=tipo;
-		
+
+		metadados[4] = tipo;
+
 		for (int i = 0; i < utilizado.length; i++) {
-			metadados[4+i]=utilizado[i];
+			metadados[4 + i] = utilizado[i];
 		}
-		
+
 		return metadados;
-		
+
 	}
 
 	public static void armazenar(int ultimoI, byte conteudo2[], int n, ArrayList<byte[]> dadosAll, int contBlocos) {
 
-		//id do container
-		byte idContainer=0;
-		//id do bloco
+		// id do container
+		byte idContainer = 0;
+		// id do bloco
 		byte id[] = new byte[3];
 		String aux = Integer.toString(contBlocos);
-		id=aux.getBytes();
+		id = aux.getBytes();
 		contBlocos++;
-		
+
 		byte dados[] = new byte[n];
 		for (int i = 0; i < conteudo2.length; i++) {
 			if (i > n - 1) {
