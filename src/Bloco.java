462ef70dccf14714ba1f6b9877cb84944bd5f72a
hanielhgc <haniel_sasuke@hotmail.com>
2018-04-09 11:00:21 -0300

    Buffer completo(?)

--- src/Bloco.java
+++ src/Bloco.java
@@ -3,11 +3,21 @@ public class Bloco {
 
 	int numero;
 
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
 	public Bloco(int numero) {
 		super();
 		this.numero = numero;
 	}
 	
-	
+	public Bloco() {
+		super();
+	}
 	
 }
--- src/Haniel.java
+++ src/Haniel.java
@@ -1,95 +1,147 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Haniel {
-	
+
 	public static void main(String[] args) {
+
+		// teste
+
+		Bloco bloco = new Bloco(0);
+		Bloco bloco2 = new Bloco(1);
+		Bloco bloco3 = new Bloco(2);
+		Bloco bloco4 = new Bloco(3);
+		Bloco bloco5 = new Bloco(4);
+
+		Bloco[] blocos = new Bloco[5];
+
+		blocos[0] = bloco;
+		blocos[1] = bloco2;
+		blocos[2] = bloco3;
+		blocos[3] = bloco4;
+		blocos[4] = bloco5;
+
+		// fim do teste
+		ArrayList<Bloco> MP = new ArrayList();
+		MP.add(bloco);
+		MP.add(bloco2);
+		MP.add(bloco3);
+		MP.add(bloco4);
+		MP.add(bloco5);
+
+		// Buffer buffer = new Buffer(5,blocos,false, true);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe o tamanho do buffer:");
+		int tamanho;
+		tamanho=scanner.nextInt();
+		
+		Buffer buffer = new Buffer(tamanho, new Bloco[tamanho], false, true);
+		buffer.blocos[0]=bloco;
+		
+		int indice = 0;
+		Bloco novoBloco = new Bloco(0);
 		
 		
-		//teste
-		
-				Bloco bloco = new Bloco(0);
-				Bloco bloco2 = new Bloco(1);
-				Bloco bloco3 = new Bloco(2);
-				Bloco bloco4 = new Bloco(3);
-				Bloco bloco5 = new Bloco(4);
-				
-				Bloco[] blocos = new Bloco[5];
-				
-				blocos[0]=bloco;
-				blocos[1]=bloco2;
-				blocos[2]=bloco3;
-				blocos[3]=bloco4;
-				blocos[4]=bloco5;
-				
-				
-				//fim do teste
-		
-		Buffer buffer = new Buffer(5,blocos,false, true);
-		Bloco novoBloco = new Bloco(10);
-		
-		
-		//antes de tudo, procurar no buffer a informação
-		boolean encontrado=false;
+
+		// antes de tudo, procurar no buffer a informação
+		boolean encontrado = false;
 		for (int i = 0; i < buffer.getBlocos().length; i++) {
-			if(buffer.getBlocos()[i]==novoBloco){
+			if(buffer.getBlocos()[i]!=null){
+			if (buffer.getBlocos()[i].getNumero() == novoBloco.getNumero()) {
+				Bloco blocoAux = new Bloco();
+				blocoAux = buffer.getBlocos()[i];
+
 				System.out.println("CACHE HIT!");
-				encontrado=true;
-				//fazer procedimento LRU para alterar as posições
-				//exibir o resultado?
+				encontrado = true;
+
+				remove(blocoAux, buffer.blocos);
+				buffer.blocos[0] = blocoAux;
+
+				// fazer procedimento LRU para alterar as posições DONE
+				// exibir o resultado? DONE
+
+				System.out.println("Conteudo do buffer:");
+				for (int j = 0; j < buffer.blocos.length; j++) {
+					if(buffer.getBlocos()[j]!=null){
+									System.out.print(buffer.getBlocos()[j].getNumero() + "		");
+					}
+					}
+
+			}
+
+		}
+		}
+
+		if (encontrado == false) {
+			// procurar na memoria principal e chamar os metodos abaixo
+			System.out.println("CACHE MISS!");
+			System.out.println("Procurando na memória principal...");
+			if (buffer.isCheio() == true) {
+
+				for (int i = 0; i < MP.size(); i++) {
+					if (MP.get(i).getNumero() == novoBloco.getNumero()) {
+						System.out.println("Encontrado!");
+
+						Bloco[] aux = new Bloco[buffer.getBlocos().length];
+
+						for (int j = 0; j < buffer.getBlocos().length - 1; j++) {
+							aux[j + 1] = buffer.getBlocos()[j];
+
+						}
+						buffer.setBlocos(aux);
+						buffer.blocos[0] = MP.get(i);
+
+					}
+
+				}
+
+			} else {
+				for (int i = 0; i < MP.size(); i++) {
+					if (MP.get(i).getNumero() == novoBloco.getNumero()) {
+						buffer.blocos[indice] = MP.get(i);
+						indice++;
+						if(indice==buffer.getBlocos().length){
+							buffer.setCheio(true);
+						}
 				
+						System.out.println("Conteudo do buffer:");
+						for (int j = 0; j < buffer.blocos.length; j++) {
+							if(buffer.getBlocos()[j]!=null){
+											System.out.print(buffer.getBlocos()[j].getNumero() + "		");
+							}
+							}
+						
+						
+						
+					}
+				}
+
+			}
+
+		}
+		
+		
+		
+		
+		
+
+	}
+
+	public static void remove(Bloco y, Bloco vetor[]) {
+		Bloco[] vetorAux = new Bloco[vetor.length];
+		int index = 1; // aparentemente isso soluciona o problema
+		for (int i = 0; i < vetor.length; i++) {
+			if(vetor[i]!=null){
+				
+			if (vetor[i].getNumero() != y.getNumero()) {
+				vetorAux[index] = vetor[i];
+				index++;
+			}
 			}
 			
 		}
-		
-		if(encontrado==false){
-			//chamar procedimentos abaixo
-		}
-		
-		
-		if(buffer.isVazio()==true){
-			buffer.blocos[0]=novoBloco;
-			
-			
-		}
-		
-		
-		if(buffer.isCheio()==false){
-			
-			Bloco[] aux = new Bloco[buffer.getBlocos().length];
-			
-			for (int i = 0; i < buffer.getBlocos().length-1; i++) {
-				aux[i+1]=buffer.getBlocos()[i];
-				
-				
-			}
-		
-			for (int i = 0; i < aux.length; i++) {
-				System.out.println("aux:"+aux[i].numero);
-				System.out.println("buffer:"+buffer.getBlocos()[i].numero);
-			}
-			//receber novo bloco em aux[0]
-			aux[0]=novoBloco;
-			
-			buffer.setBlocos(aux);
-			//checar se o buffer está cheio e se estiver, setá-lo
-			
-			
-			
-		}else{
-			//em caso de buffer cheio
-			
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		vetor = vetorAux;
 	}
 
 }
